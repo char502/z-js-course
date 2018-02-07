d8b5697b43ca50d65e62232b5173022a74d98f2b
char502 <char502@hotmail.com>
2018-02-07 11:56:32 +0000

    command 2

--- js/main.js
+++ js/main.js
@@ -6,8 +6,11 @@
 // const logger = (arg1) => console.log(arg1)
 // logger("this is a test")
 
-// const addNums = (num1, num2) => num1 + num2;
-// console.log(addNums(5, 6));
+// const addNums = (num1, num2) => num1 + num2
+// console.log(addNums(5, 6))
 
-const multiplyNums = (num1, num2) => num1 * num2
-console.log(multiplyNums(2, 3));
+// const multiplyNums = (num1, num2) => num1 * num2
+// console.log(multiplyNums(2, 3))
+
+const divideNums = (num1, num2) => num1 / num2
+console.log(divideNums(10, 2))
