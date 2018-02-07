3415f4a48cf71afff931e58d8cca8410c7ed52fb
char502 <char502@hotmail.com>
2018-02-07 11:43:01 +0000

    ES6 practice

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// alert('Your JavaScript is linked')
+
+// const ten = _ => {console.log(10)}
+// ten()
+
+// const logger = (arg1) => console.log(arg1)
+// logger("this is a test")
+
+const addNums = (num1, num2) => num1 + num2;
+console.log(addNums(5, 6));
+
+// const multiplyNums = (num1, num2) => num1 * num2
+// console.log(multiplyNums(2, 3));
