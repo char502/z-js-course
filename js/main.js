e2394d0046fe8ef18542993c9839021a297818ed
char502 <char502@hotmail.com>
2018-02-07 11:53:45 +0000

    command line git

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@
 // const logger = (arg1) => console.log(arg1)
 // logger("this is a test")
 
-const addNums = (num1, num2) => num1 + num2;
-console.log(addNums(5, 6));
+// const addNums = (num1, num2) => num1 + num2;
+// console.log(addNums(5, 6));
 
-// const multiplyNums = (num1, num2) => num1 * num2
-// console.log(multiplyNums(2, 3));
+const multiplyNums = (num1, num2) => num1 * num2
+console.log(multiplyNums(2, 3));
